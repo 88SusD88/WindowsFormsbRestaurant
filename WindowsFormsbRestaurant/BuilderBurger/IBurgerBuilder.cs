26fb266b12159b71cce69c3763a0ad28adabf731
88SusD88 <is25suslinikovda@artcollege.ru>
2024-10-24 11:00:06 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsbRestaurant/BuilderBurger/IBurgerBuilder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsFormsbRestaurant.BuilderBurger;
+using WindowsFormsbRestaurant.DBCon;
+
+namespace WindowsFormsbRestaurant.BuilderBurger
+{
+    internal interface IBurgerBuilder
+    {
+        IBurgerBuilder AddCheese();
+        IBurgerBuilder AddBacon();
+        IBurgerBuilder AddLetuce();
+        IBurgerBuilder AddTomato();
+        IBurgerBuilder AddPickles();
+        Burgers GetBurgers();
+
+    }
+}
---
+++ WindowsFormsbRestaurant/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace WindowsFormsbRestaurant
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.comboBoxBurger = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(910, 430);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonAdd.Location = new System.Drawing.Point(699, 448);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(223, 33);
+            this.buttonAdd.TabIndex = 1;
+            this.buttonAdd.Text = "Добавить";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // comboBoxBurger
+            // 
+            this.comboBoxBurger.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxBurger.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBoxBurger.FormattingEnabled = true;
+            this.comboBoxBurger.Items.AddRange(new object[] {
+            "Стандартный бургер",
+            "Бургер с беконом"});
+            this.comboBoxBurger.Location = new System.Drawing.Point(12, 448);
+            this.comboBoxBurger.Name = "comboBoxBurger";
+            this.comboBoxBurger.Size = new System.Drawing.Size(223, 33);
+            this.comboBoxBurger.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(934, 508);
+            this.Controls.Add(this.comboBoxBurger);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.ComboBox comboBoxBurger;
+    }
+}
+
